2f663b12929a9b41f75810a3f6dc44eddcb98565
Bryan Ash <bryan.a.ash@gmail.com>
2011-01-03 18:54:19 -0500

    Init

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source :gemcutter
+
+gem 'haml'
+gem 'pg'
+gem 'rails'
+gem 'simple_autocomplete'
+
+group :development do
+  gem 'heroku_san'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleAutocompleteDemo::Application.config.secret_token = '8002bae3ba4d84bc284b26a0d14ade64957b4aaee911485cc94497d02f3dcd5683774ea06e677b08907937daca286ba0ff2aa16745934c09bccf4d5d157617b6'
